b78fb4ebb6300dcbde7a27054fc185dfd1cfd3fa
a18006967 <steffen.alvarez@etu.univ-amu.fr>
2019-09-16 14:05:39 +0200

    phpinfo()

---
+++ .inc.php
@@ -0,0 +1,4 @@
+<?php
+    include 'utils.inc.php';
+    phpinfo()
+?>
---
+++ utils.inc.php
@@ -0,0 +1,10 @@
+<?php
+start_page('titre');
+function end_page()
+{
+    echo '</body>
+                  </html>';
+}
+echo '<hr/><br/><strong>Test</strong><br/><hr/>';
+end_page();
+?>
