5e43d436a27a54e78f0c4ca70ac9ef9ad7266418
a18006967 <steffen.alvarez@etu.univ-amu.fr>
2019-09-16 14:39:37 +0200

    html

--- index.php
+++ index.php
@@ -7,19 +7,21 @@
     <body>
     <form>
             <input type="text" name="id" placeholder="Identifiant">
-            <input type="radio" name="homme">
-            <input type="radio" name="femme">
+            <input type="radio" id="homme" name="homme">
+            <label for="homme">homme</label>
+            <input type="radio" id=femme" name="femme">
+            <label for="femme">femme</label>
             <input type="text" name="mail" placeholder="E-mail">
             <input type="password" name="mpd" placeholder="Mot de passe">
             <input type="password" name="verif" placeholder="Verifiez mdp">
             <input type="text" name="tel" placeholder="Telephone">
             <select id="Pays">
-                <option value=""> Choisir pays></option>
-                <option value="France" name="france">France></option>
-                <option value="Allemagne" name="allemagne">Allemagne></option>
+                <option value=""> Choisir pays</option>
+                <option value="France" name="france">France</option>
+                <option value="Allemagne" name="allemagne">Allemagne</option>
             </select>
             <input type="checkbox" id="conditions" name="conditions">
-            <label for="conditions">Conditions Generales></label>
+            <label for="conditions">Conditions Generales</label>
             <input type="submit" value="envoyer">
     </form>
     </body>
