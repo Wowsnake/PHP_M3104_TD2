0c4e6737f83f3a316ada1ebc24a08577e46ed199
a18006967 <steffen.alvarez@etu.univ-amu.fr>
2019-09-16 14:12:03 +0200

    start

--- utils.inc.php
+++ utils.inc.php
@@ -1,10 +1,12 @@
 <?php
-start_page('titre');
+function start_page($title)
+{
+    echo ' <!DOCTYPE html> <lang="fr">
+ <head><title>' . PHP_EOL . $title . '</title></head><body>' . PHP_EOL;
+};
 function end_page()
 {
     echo '</body>
                   </html>';
 }
-echo '<hr/><br/><strong>Test</strong><br/><hr/>';
-end_page();
 ?>
