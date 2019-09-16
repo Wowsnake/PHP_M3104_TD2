43c30114cb134104508223414b7c426aabff9603
a18006967 <steffen.alvarez@etu.univ-amu.fr>
2019-09-16 17:21:23 +0200

    html

--- base.php
+++ base.php
@@ -19,7 +19,7 @@ if(!($dbResult=mysqli_query($dbLink, $query)))
 
 while($dbRow = mysqli_fetch_assoc ($dbResult )){
     echo $dbRow['id'].'<br/>';
-    echo $dbRow['email'].'<br/>';
+    echo $dbRow['mail'].'<br/>';
     echo $dbRow['date'].'<br/>';
     echo'<br/><br/>';
 }
