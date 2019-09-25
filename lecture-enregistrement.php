ecc41379e699b1312537604568261f835e85c938
a18006967 <steffen.alvarez@etu.univ-amu.fr>
2019-09-25 09:21:23 +0200

    exo 2.2 fin

---
+++ lecture-enregistrement.php
@@ -0,0 +1,18 @@
+<?php
+    include 'utils.inc.php';
+    start_page('Lire Enregistrements');
+    $file = 'data.txt';
+    if(!($file = fopen($file, 'r'))) {
+        echo 'Erreur de lecture';
+        exit();
+    }
+    echo 'Liste des utilisateurs : <br>';
+    $cpt = 1;
+    while($line = fgets($file, 255))
+    {
+        echo 'Utilisateur n ' . $cpt . ' : ' . $line . '<br/>';
+        ++$cpt;
+    }
+    fclose($file);
+    end_page();
+?>
